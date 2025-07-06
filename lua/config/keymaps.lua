28f277287922f1953cbbc43357c2012f3dabbcbf
zhangsc <shicheng@xreal.com>
2025-07-06 14:23:02 +0800

    feat: update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- map
+local map = vim.keymap.set
+-- buffers
+map("n", "<C-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer", remap = true })
+map("n", "<C-l>", "<cmd>bnext<cr>", { desc = "Next Buffer", remap = true })
+-- windows
+map("n", "<space><space>", "<C-w><C-w>", { desc = "Go to Next Window" })
